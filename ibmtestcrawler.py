deb525301e0028a5b4837446c4f9247251a7df43
andretaff <andretaff@gmail.com>
2019-02-24 01:07:21 -0300

    Ajuste

--- ibmtestcrawler.py
+++ ibmtestcrawler.py
@@ -36,7 +36,7 @@ def my_form_post():
 			if sUrl not in ('_id','_rev'):
 				items.append([sUrl,iDepth])
 	items = sorted(items, key=lambda item: item[1]) 
-	return render_template("result.html", result = items)
+	return render_template("result.html", url=url,depth=depth+1, result = items)
 	
 if __name__ == '__main__':
 	port = int(os.getenv('PORT', 8000))
