8e2d959da1057d81ec938083a6f199b376fc187a
andretaff <andretaff@gmail.com>
2019-02-24 01:24:09 -0300

    Novos testes

--- tests/UrlRetriever-testing.py
+++ tests/UrlRetriever-testing.py
@@ -2,16 +2,21 @@ import unittest
 import UrlRetriever
 
 class TestUrlRetriever(unittest.TestCase):
-	def test_retrieveUrl(self):
+	def test_retrieveFakeUrl(self):
 		cUrlRetriever = UrlRetriever.tUrlRetriever('http://www.fakeurl',0)
 		mLinks = cUrlRetriever.retrieveURLLinks()
 		self.assertTrue(mLinks == {})
-		
+
+	def test_emptywebpage(self):
+		cUrlRetriever = UrlRetriever.tUrlRetriever('http://hmpg.net/',3) #The end of the internet webpage
+		mLinks = cUrlRetriever.retrieveURLLinks()
+		self.assertTrue(mLinks == {})
+
+	def test_retrieveUrl(self):
 		cUrlRetriever = UrlRetriever.tUrlRetriever('http://www.google.com',0)
 		mLinks = cUrlRetriever.retrieveURLLinks()
 		self.assertFalse(mLinks == {})
 		for sLink, iDepth in mLinks.items():
-			print ('Retrieved',sLink)
 			self.assertTrue(sLink.startswith('http'))
 			self.assertTrue(iDepth>=0)
 
